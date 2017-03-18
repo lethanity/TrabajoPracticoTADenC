8eaeb89b33985e372d72ad580bb6efa18e3ea241
Lethanity <manupedr@gmail.com>
2017-03-18 17:18:20 -0300

    Complex Number

--- main.c
+++ main.c
@@ -2,13 +2,33 @@
 #include "ComplexNumber.h"
 
 int main(){
-    ComplexNumber* c = createComplexNumber(3,4);
-    ComplexNumber* c1 = createComplexNumber(4,5);
+
+    //User input.
+    printf("Input first complex:\n");
+    printf("Real: ");
+    int r1;
+    scanf("%d",&r1);
+    printf("\nImaginary: ");
+    int i1;
+    scanf("%d",&i1);
+    printf("\nInput second complex:\n");
+    printf("Real: ");
+    int r2;
+    scanf("%d",&r2);
+    printf("\nImaginary: ");
+    int i2;
+    scanf("%d",&i2);
+    printf("\n\n");
+
+    //Test.
+    ComplexNumber* c = createComplexNumber(r1,i1);
+    ComplexNumber* c1 = createComplexNumber(r2,i2);
     ComplexNumber* s = sum(c,c1);
     ComplexNumber* p = product(c,c1);
     ComplexNumber* sub = subtract(c,c1);
     int abs = absoluteValue(c);
     int eq = equals(c,c1);
+
     printf("%d+(%di) + %d+(%di) = %d+(%di)\n", c->real, c->imaginary, c1->real,
            c1->imaginary, s->real, s->imaginary);
     printf("%d+(%di) - %d+(%di) = %d+(%di)\n", c->real, c->imaginary, c1->real,
@@ -18,5 +38,6 @@ int main(){
     printf("|%d+(%di)| = %d\n", c->real, c->imaginary, abs);
     printf("%d+(%di) equals %d+(%di) = %d\n", c->real, c->imaginary, c1->real,
            c1->imaginary, eq);
+
     return 0;
 }
